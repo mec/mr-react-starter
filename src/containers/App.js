39a60d575cb5652e550f1daeae9473dea76a9c10
Mec Rawlings <mec.rawlings@gmail.com>
2017-10-04 22:58:47 +0100

    Add ESLint and use semistandard and react.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Message from '../components/message';
 
 class App extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       message: 'hello world.'
     };
   }
-  render() {
+  render () {
     return (
       <Message message={this.state.message}/>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './containers/App.js'
+import App from './containers/App.js';
 
 ReactDOM.render(
   <App/>,
